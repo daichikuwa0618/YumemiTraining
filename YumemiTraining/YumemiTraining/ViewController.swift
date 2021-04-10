3c6ed7348b56bb3e1526e011995e9e86a972c866
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-04-10 17:38:59 +0900

    add: Empty project

---
+++ YumemiTraining/YumemiTraining/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  YumemiTraining
+//
+//  Created by Daichi Private on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
