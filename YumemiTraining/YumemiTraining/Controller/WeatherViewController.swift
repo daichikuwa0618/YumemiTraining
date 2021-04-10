8955c3bdedc86626ae56c974a2ebe3b75b9a0375
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-04-10 23:14:49 +0900

    fix: ファイル名とクラス名の不一致を解消し、 Daichi Private → Daichi Hayashi に修正

--- YumemiTraining/YumemiTraining/Controller/WeatherViewController.swift
+++ YumemiTraining/YumemiTraining/Controller/WeatherViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  YumemiTraining
 //
-//  Created by Daichi Private on 2021/04/10.
+//  Created by Daichi Hayashi on 2021/04/10.
 //
 
 import UIKit
--- YumemiTraining/YumemiTraining/SceneDelegate.swift
+++ YumemiTraining/YumemiTraining/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  YumemiTraining
 //
-//  Created by Daichi Private on 2021/04/10.
+//  Created by Daichi Hayashi on 2021/04/10.
 //
 
 import UIKit
