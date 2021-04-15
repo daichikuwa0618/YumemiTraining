cc31af6ca885c5d25de82b4cf43c0b278261d905
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-04-16 06:09:36 +0900

    add: 3 つの天気の enum Weather, Weather から View の状態を作る struct, YumemiWeather から Weather を fetch する class を追加

---
+++ YumemiTraining/YumemiTraining/Model/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  YumemiTraining
+//
+//  Created by Daichi Hayashi on 2021/04/13.
+//
+
+import Foundation
+
+enum Weather {
+    case sunny
+    case rainy
+    case cloudy
+}
---
+++ YumemiTraining/YumemiTraining/Model/WeatherFetcher.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherFetcher.swift
+//  YumemiTraining
+//
+//  Created by Daichi Hayashi on 2021/04/13.
+//
+
+import Foundation
+
+import YumemiWeather
+
+protocol WeatherFetcherProtocol {
+    func fetch() -> Weather
+}
+
+class WeatherFetcher: WeatherFetcherProtocol {
+    func fetch() -> Weather {
+        let fetchedString: String = YumemiWeather.fetchWeather()
+        let weather: Weather = {
+            switch fetchedString {
+            case "sunny":
+                return .sunny
+
+            case "cloudy":
+                return .cloudy
+
+            case "rainy":
+                return .rainy
+
+            default:
+                assertionFailure("unexpected string was returned.")
+                return .cloudy
+            }
+        }()
+
+        return weather
+    }
+}
