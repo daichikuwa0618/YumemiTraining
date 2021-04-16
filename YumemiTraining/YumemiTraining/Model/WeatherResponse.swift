792cb832379fb32f0d42e446c6759fa1528ba018
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-04-17 07:25:37 +0900

    add: Json レスポンスを構造体で定義

---
+++ YumemiTraining/YumemiTraining/Model/WeatherResponse.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherResponse.swift
+//  YumemiTraining
+//
+//  Created by Daichi Hayashi on 2021/04/17.
+//
+
+import Foundation
+
+struct WeatherResponse {
+    let weather: Weather
+    let maxTemperature: Int
+    let minTemperature: Int
+    let date: Date
+}
